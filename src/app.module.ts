5ea40b8590323867f8af573da6a5bfa4ac76c17c
Marco Cárdenas <120759277+Marco-Cardenas@users.noreply.github.com>
2025-06-06 20:04:39 -0400

    borrando una direccion inexistente en el archivo src/app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { ServiceController } from './service/service.controller';
 
 @Module({
   imports: [AuthModule],
-  controllers: [AppController, ServiceController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
