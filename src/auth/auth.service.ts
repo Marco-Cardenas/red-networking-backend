6a9aa3432f93cbc68508e139e7aaaf99cae5b523
jgarcia691 <jagarciar.info@gmail.com>
2025-06-07 19:36:16 -0400

    Autenticacion temporal

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  login(username: string, password: string): { access_token: string } | null {
+    // Credenciales temporales
+    if (username === 'admin' && password === '1234') {
+      return { access_token: 'Autenticado Fake' };
+    }
+    return null;
+  }
+}
