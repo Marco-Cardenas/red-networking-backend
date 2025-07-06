67adc718ce097550654be40eb0417cc6302bd2a9
jgarcia691 <jagarciar.info@gmail.com>
2025-07-06 10:47:51 -0400

    ajuste de tamaño de peticion

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express'; 
 
 async function bootstrap() {
 
 
 
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
 
   app.useGlobalPipes(
     new ValidationPipe({
